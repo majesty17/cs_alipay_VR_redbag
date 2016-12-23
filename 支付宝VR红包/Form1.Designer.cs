4baaa342c046401b639f33117675745a562dc702
Majesty <a-o@qq.com>
2016-12-24 03:09:37 +0800

    first commit

---
+++ 支付宝VR红包/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace 支付宝VR红包
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "图片文件|*.png;*.jpg";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "打开";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(107, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(647, 497);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 499);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 支付宝VR红包/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace 支付宝VR红包
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
+                
+                string filename=openFileDialog1.FileName;
+                System.Drawing.Image initImage = System.Drawing.Image.FromFile(filename);
+
+                //新建一个bmp图片
+                System.Drawing.Image bitmap = new System.Drawing.Bitmap(420, 420);
+                //新建一个画板
+                Graphics g = System.Drawing.Graphics.FromImage(bitmap);
+                //设置高质量插值法
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                //设置高质量,低速度呈现平滑程度
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                //清空画布并以透明背景色填充
+                g.Clear(Color.Transparent);
+                //在指定位置并且按指定大小绘制原图片的指定部分
+                g.DrawImage(initImage, new Rectangle(0, 0, 420, 420),
+                    new Rectangle(330, 1071, 420, 420),
+                    GraphicsUnit.Pixel);
+
+
+
+                for (int i = 0; i < 27; i++) {
+                    //g.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 11 + 15 * i, 420, 8));
+                    g.DrawImage(bitmap, new Rectangle(0, 11 + 15 * i, 420, 4),
+                    new Rectangle(0, 11 + 15 * i - 3, 420, 4),
+                    GraphicsUnit.Pixel);
+                    g.DrawImage(bitmap, new Rectangle(0, 11 + 15 * i+4, 420, 4),
+                    new Rectangle(0, 11 + 15 * i +8, 420, 4),
+                    GraphicsUnit.Pixel);
+                }
+
+                try
+                {
+                    //以jpg格式保存缩略图
+                    bitmap.Save(@"C:\1.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+                    pictureBox1.Image = Image.FromFile(@"C:\1.jpg");
+                }
+                catch (System.Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    initImage.Dispose();
+                    bitmap.Dispose();
+                    g.Dispose();
+                }
+
+            }
+        }
+    }
+}
