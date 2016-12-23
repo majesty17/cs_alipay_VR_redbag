8321d13d4bde1dec3e9398f8139c3ce066058471
Majesty <a-o@qq.com>
2016-12-24 07:51:03 +0800

    调整了算法，增加了可配半径的高斯模糊

--- 支付宝VR红包/Form1.Designer.cs
+++ 支付宝VR红包/Form1.Designer.cs
@@ -28,15 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.Filter = "图片文件|*.png;*.jpg";
             // 
             // button1
             // 
@@ -56,31 +54,62 @@
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(107, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(647, 497);
+            this.pictureBox1.Size = new System.Drawing.Size(725, 725);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "高斯模糊半径";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 79);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.ReadOnly = true;
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
             // 
             // Form1
             // 
             this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(754, 499);
+            this.ClientSize = new System.Drawing.Size(832, 727);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
 
--- 支付宝VR红包/Form1.cs
+++ 支付宝VR红包/Form1.cs
@@ -6,11 +6,26 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Diagnostics;
+using System.Reflection;
+using System.Drawing.Drawing2D;
+
 
 namespace 支付宝VR红包
 {
     public partial class Form1 : Form
     {
+        [DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = true)]
+        private static extern void CopyMemory(IntPtr Dest, IntPtr src, int Length);                 // Marshal.Copy 居然没有从一个内存地址直接复制到另外一个内存的重载函数        
+
+        private Bitmap Bmp=null,b;
+        private IntPtr ImageCopyPointer, ImagePointer;
+        private int DataLength;
+
+
+
         public Form1()
         {
             InitializeComponent();
@@ -18,55 +33,68 @@ namespace 支付宝VR红包
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (openFileDialog1.ShowDialog() == DialogResult.OK) {
-                
-                string filename=openFileDialog1.FileName;
-                System.Drawing.Image initImage = System.Drawing.Image.FromFile(filename);
-
-                //新建一个bmp图片
-                System.Drawing.Image bitmap = new System.Drawing.Bitmap(420, 420);
-                //新建一个画板
-                Graphics g = System.Drawing.Graphics.FromImage(bitmap);
-                //设置高质量插值法
-                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
-                //设置高质量,低速度呈现平滑程度
-                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
-                //清空画布并以透明背景色填充
-                g.Clear(Color.Transparent);
-                //在指定位置并且按指定大小绘制原图片的指定部分
-                g.DrawImage(initImage, new Rectangle(0, 0, 420, 420),
-                    new Rectangle(330, 1071, 420, 420),
-                    GraphicsUnit.Pixel);
-
-
-
-                for (int i = 0; i < 27; i++) {
-                    //g.FillRectangle(new SolidBrush(Color.White), new Rectangle(0, 11 + 15 * i, 420, 8));
-                    g.DrawImage(bitmap, new Rectangle(0, 11 + 15 * i, 420, 4),
-                    new Rectangle(0, 11 + 15 * i - 3, 420, 4),
-                    GraphicsUnit.Pixel);
-                    g.DrawImage(bitmap, new Rectangle(0, 11 + 15 * i+4, 420, 4),
-                    new Rectangle(0, 11 + 15 * i +8, 420, 4),
-                    GraphicsUnit.Pixel);
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Bitmap files (*.bmp)|*.bmp|Jpeg files (*.jpg)|*.jpg|Png files (*.png)|*.png|All valid files (*.bmp/*.jpg/*.png)|*.bmp;*.jpg;*.png";
+            openFileDialog.FilterIndex = 4;
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (Bmp != null)
+                {
+                    Bmp.Dispose();
+                    Marshal.FreeHGlobal(ImageCopyPointer);
                 }
-
                 try
                 {
-                    //以jpg格式保存缩略图
-                    bitmap.Save(@"C:\1.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-                    pictureBox1.Image = Image.FromFile(@"C:\1.jpg");
+                    Bmp = (Bitmap)Bitmap.FromFile(openFileDialog.FileName);
+                    BitmapData BmpData = new BitmapData();
+                    Bmp.LockBits(new Rectangle(0, 0, Bmp.Width, Bmp.Height), ImageLockMode.ReadWrite, Bmp.PixelFormat, BmpData);    // 用原始格式LockBits,得到图像在内存中真正地址，这个地址在图像的大小，色深等未发生变化时，每次Lock返回的Scan0值都是相同的。
+                    ImagePointer = BmpData.Scan0;                            //  记录图像在内存中的真正地址
+                    DataLength = BmpData.Stride * BmpData.Height;           //  记录整幅图像占用的内存大小
+                    ImageCopyPointer = Marshal.AllocHGlobal(DataLength);    //  直接用内存数据来做备份，AllocHGlobal在内部调用的是LocalAlloc函数
+                    CopyMemory(ImageCopyPointer, ImagePointer, DataLength); //  这里当然也可以用Bitmap的Clone方式来处理，但是我总认为直接处理内存数据比用对象的方式速度快。
+                    Bmp.UnlockBits(BmpData);
+                    //pictureBox1.Image = Bmp;
                 }
-                catch (System.Exception ex)
+                catch (Exception d)
                 {
-                    throw ex;
-                }
-                finally
-                {
-                    initImage.Dispose();
-                    bitmap.Dispose();
-                    g.Dispose();
+                    MessageBox.Show(d.Message);
                 }
 
+                numericUpDown1_ValueChanged(sender, e);
+            }
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            if (Bmp == null)
+                return;
+            b = new Bitmap(420, 420);
+            Graphics g = Graphics.FromImage(b);
+            // 插值算法的质量
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(Bmp, new Rectangle(0, 0, 420, 420),
+                new Rectangle(330, 1071, 420, 420),
+                GraphicsUnit.Pixel);
+
+            for (int i = 0; i < 27; i++)
+            {
+                //对于一个8px高度的黑线，上下各取出4px的内容来填充
+                g.DrawImage(Bmp, new Rectangle(0, 11 + 15 * i, 420, 4),
+                new Rectangle(330 + 0, 1071 + 11 + 15 * i - 3, 420, 4),
+                GraphicsUnit.Pixel);
+                g.DrawImage(Bmp, new Rectangle(0, 11 + 15 * i + 4, 420, 4),
+                new Rectangle(330 + 0, 1071 + 11 + 15 * i + 8, 420, 4),
+                GraphicsUnit.Pixel);
+            }
+            Rectangle ret = new Rectangle(0, 0, 420, 420);
+            double rad = Convert.ToDouble(numericUpDown1.Text);
+            b.GaussianBlur(ref ret, (float)rad, false);
+            pictureBox1.Image = b;
+            g.Dispose();
+            if (rad > 255) {
+                numericUpDown1.Value = 255;
             }
         }
     }
